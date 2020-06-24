1f851b10223ab5c46a5135bc8eb97585faee1bb4
almasmoot <smo15001@byui.edu>
2020-06-24 13:35:52 -0600

    Added new goal Layout

---
+++ app/src/main/java/com/example/teamproject/NewGoals.java
@@ -0,0 +1,73 @@
+package com.example.teamproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.RadioButton;
+import android.widget.TextView;
+
+import java.util.Calendar;
+
+public class NewGoals extends AppCompatActivity {
+
+    Button selectDate;
+    TextView date;
+    DatePickerDialog datePickerDialog;
+    int year;
+    int month;
+    int dayOfMonth;
+    Calendar calendar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        selectDate = findViewById(R.id.btnDate);
+        date = findViewById(R.id.tvSelectedDate);
+
+        selectDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                calendar = Calendar.getInstance();
+                year = calendar.get(Calendar.YEAR);
+                month = calendar.get(Calendar.MONTH);
+                dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
+                datePickerDialog = new DatePickerDialog(NewGoals.this,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                                date.setText(day + "/" + (month + 1) + "/" + year);
+                            }
+                        }, year, month, dayOfMonth);
+                datePickerDialog.getDatePicker().setMinDate(System.currentTimeMillis());
+                datePickerDialog.show();
+            }
+        });
+    }
+
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.radioDaily:
+                if (checked)
+
+                    break;
+            case R.id.radioWeekly:
+                if (checked)
+
+                    break;
+            case R.id.radioOneTime:
+                if (checked)
+
+                    break;
+        }
+    }
+}
