fdb479d7d6f034eba41a08b9f6c5177be22cfcc6
Alex Krentz <kre19005@byui.edu>
2020-06-25 21:01:31 -0600

    added menu navigation

--- app/src/main/java/com/example/teamproject/MainActivity.java
+++ app/src/main/java/com/example/teamproject/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.teamproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -24,26 +25,36 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    // Creates the menu
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu, menu);
         return true;
     }
+
+
+    // Navigation with the menu
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+
         switch (item.getItemId()) {
             case R.id.newGoal:
-                Toast.makeText(this, "Item 1 selected", Toast.LENGTH_SHORT).show();
+                Intent intent1 = new Intent(this, NewGoals.class);
+                this.startActivity(intent1);
                 return true;
             case R.id.existingGoals:
-                Toast.makeText(this, "Item 2 selected", Toast.LENGTH_SHORT).show();
+                Intent intent2 = new Intent(this, FirstFragment.class);
+                this.startActivity(intent2);
                 return true;
             case R.id.progress:
-                Toast.makeText(this, "Item 3 selected", Toast.LENGTH_SHORT).show();
+                Intent intent3 = new Intent(this, ProgressGraph.class);
+                this.startActivity(intent3);
                 return true;
             case R.id.friendProgress:
-                Toast.makeText(this, "Item 3 selected", Toast.LENGTH_SHORT).show();
+                Intent intent4 = new Intent(this, SecondFragment.class);
+                this.startActivity(intent4);
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
