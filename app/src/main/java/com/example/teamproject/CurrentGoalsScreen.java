36c25c34b165824a08b7d721cf2a384708f46981
cortsmeltz <csmeltz2@byui.edu>
2020-07-06 19:25:20 -0600

    Added screen for current goals

---
+++ app/src/main/java/com/example/teamproject/CurrentGoalsScreen.java
@@ -0,0 +1,14 @@
+package com.example.teamproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CurrentGoalsScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_current_goals_screen);
+    }
+}
