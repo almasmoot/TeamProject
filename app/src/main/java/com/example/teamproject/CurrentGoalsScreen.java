b8f7084cb4739646075afb0e0460a950f43f6d20
Brennan Smeltzer <sme18001@byui.edu>
2020-07-17 17:51:37 -0600

    CurrentGoals Updates

--- app/src/main/java/com/example/teamproject/CurrentGoalsScreen.java
+++ app/src/main/java/com/example/teamproject/CurrentGoalsScreen.java
@@ -1,9 +1,17 @@
 package com.example.teamproject;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.ListView;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 
@@ -15,6 +23,7 @@ public class CurrentGoalsScreen extends AppCompatActivity {
     //initialize list here
     //GoalList.GoalAdapter goalAdapter;
     public static final String EXTRA_MESSAGE = "";
+    DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,12 +32,40 @@ public class CurrentGoalsScreen extends AppCompatActivity {
         lv = (ListView)findViewById(R.id.listview);
         displayGoalList();
 
+
     }
 
     private void displayGoalList(){
-        goalList = (ArrayList)FirebaseLists.getFirebaseList(); //assume that getFireBaseList will be fixed
+        //goalList = (ArrayList)FirebaseLists.getFirebaseList(); //assume that getFireBaseList will be fixed
+        goalList = new ArrayList<Goal>();
+        databaseReference = FirebaseDatabase.getInstance().getReference("goals");
         glAdapter = new GoalAdapter(goalList, this);
-        lv.setAdapter(glAdapter);
+        databaseReference.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                Goal value=snapshot.getValue(Goal.class);
+                goalList.add(value);
+
+                lv.setAdapter(glAdapter);
+
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {}
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {}
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {}
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {}
+        });
+
+
+
+
     }
 
 
