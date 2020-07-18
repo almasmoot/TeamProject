329b183c8aea0797c8704690d4257aa96ec11bbd
Alex Krentz <kre19005@byui.edu>
2020-07-17 18:33:37 -0600

    fixed current goal screen so they can be clicked

--- app/src/main/java/com/example/teamproject/CurrentGoalsScreen.java
+++ app/src/main/java/com/example/teamproject/CurrentGoalsScreen.java
@@ -5,6 +5,9 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import com.google.firebase.database.ChildEventListener;
@@ -17,52 +20,57 @@ import java.util.ArrayList;
 
 public class CurrentGoalsScreen extends AppCompatActivity {
 
-    ListView lv;
-    ArrayList<Goal> goalList;
-    GoalAdapter glAdapter;
-    //initialize list here
-    //GoalList.GoalAdapter goalAdapter;
-    public static final String EXTRA_MESSAGE = "";
+    ArrayList<String> arrayList = new ArrayList<>();
+    ArrayList<String> selectedItems;
+    ArrayAdapter<String> arrayAdapter;
     DatabaseReference databaseReference;
+    ListView listView;
+    public static final String EXTRA_MESSAGE = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_current_goals_screen);
-        lv = (ListView)findViewById(R.id.listview);
-        displayGoalList();
-
-
-    }
-
-    private void displayGoalList(){
-        //goalList = (ArrayList)FirebaseLists.getFirebaseList(); //assume that getFireBaseList will be fixed
-        goalList = new ArrayList<Goal>();
-        databaseReference = FirebaseDatabase.getInstance().getReference("goals");
-        glAdapter = new GoalAdapter(goalList, this);
+        selectedItems=new ArrayList<String>();
+        databaseReference=FirebaseDatabase.getInstance().getReference("goals");
+        listView=(ListView) findViewById(R.id.listview);
+        arrayAdapter = new ArrayAdapter<String>(CurrentGoalsScreen.this,R.layout.checkable_list_layout,R.id.txt_title,arrayList);
+        listView.setAdapter(arrayAdapter);
         databaseReference.addChildEventListener(new ChildEventListener() {
             @Override
             public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-                Goal value=snapshot.getValue(Goal.class);
-                goalList.add(value);
+                String value=snapshot.getValue(Goal.class).toString();
+                arrayList.add(value);
+                arrayAdapter.notifyDataSetChanged();
+            }
 
-                lv.setAdapter(glAdapter);
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
 
             }
 
             @Override
-            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {}
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+            }
 
             @Override
-            public void onChildRemoved(@NonNull DataSnapshot snapshot) {}
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
 
             @Override
-            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {}
+            public void onCancelled(@NonNull DatabaseError error) {
 
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {}
+            }
         });
-
+    listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//            TODO what happens when you click
+        }
+    });
+    }
 
 
 
@@ -70,4 +78,3 @@ public class CurrentGoalsScreen extends AppCompatActivity {
 
 
 
-}
