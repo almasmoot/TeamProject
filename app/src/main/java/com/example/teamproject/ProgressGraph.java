1b403bc5f779e1ed45a2319291bd2dd498cebdb5
Brennan Smeltzer <sme18001@byui.edu>
2020-07-18 19:55:32 -0600

    Decided to remove ProgressGraph from the app, it will not work with the current database model

--- app/src/main/java/com/example/teamproject/ProgressGraph.java
+++ app/src/main/java/com/example/teamproject/ProgressGraph.java
@@ -9,9 +9,6 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.widget.ArrayAdapter;
-import android.widget.ListAdapter;
-import android.widget.Spinner;
 
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.data.Entry;
@@ -24,57 +21,57 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-
 import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.List;
 
+
+//This graph does not work, it was fundamentally flawed as the database model was not set up for it.
+//We decided to cut scope
 public class ProgressGraph extends AppCompatActivity {
 
+    //global variables
     LineChart goalChart;
     String goalString;
     ArrayList<Entry> dataVals = new ArrayList<Entry>();
+
+    //onCreate function, creates the graph after populating the global list with data
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_progress_graph);
         goalChart=(LineChart) findViewById(R.id.linechart);
-        //put data in graph
+        //get the intent
         Intent intent = getIntent();
         goalString = intent.getStringExtra(CurrentGoalsScreen.EXTRA_MESSAGE);
+        //calls function to fill list with data entries
         LineDataSet lineDataSet1 = new LineDataSet(dataValues1(), "Data Set 1");
 
         ArrayList<ILineDataSet> dataSets = new ArrayList<>();
         dataSets.add(lineDataSet1);
 
+        //put data entries into the graph
         LineData data = new LineData(dataSets);
         goalChart.setData(data);
         goalChart.invalidate();
-
-
-
     }
 
     private ArrayList<Entry> dataValues1()
     {
+        //function variables, accesses firebase
         DatabaseReference databaseReference;
         databaseReference=FirebaseDatabase.getInstance().getReference("goals");
 
-        //for loop
+        //adds data point from specific goal
         databaseReference.addChildEventListener(new ChildEventListener() {
                 @Override
                 public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-                    //check for specific goalname that was passed from the call
-                    //get intent and stuff
+                    //check for specific goal that was passed from the call
                     //Calendar goalDate = Calendar.getInstance();
                     Goal temp = snapshot.getValue(Goal.class);
                     if(goalString == temp.toString()) {
                         //goalDate.setTimeInMillis(snapshot.getValue(Goal.class).getDate().getTime());
+
                         dataVals.add(new Entry ((int)temp.getDate().getTime()/86400000, temp.getAccomplished()));
                     }
-                    //String value=snapshot.getValue(Goal.class).toString();
-                    //arrayList.add(value);
-                    //arrayAdapter.notifyDataSetChanged();
                 }
                 @Override
                 public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {}
@@ -89,15 +86,14 @@ public class ProgressGraph extends AppCompatActivity {
         return dataVals;
     }
 
-
-
+    //Menu selector
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu, menu);
         return true;
     }
-
+//items in the menu
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
 
