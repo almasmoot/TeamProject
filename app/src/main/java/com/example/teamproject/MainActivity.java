5b1c8f6c3c2af70c78899175d328c2fc7504eff2
Alex Krentz <kre19005@byui.edu>
2020-07-18 19:11:09 -0600

    worked on deleting from firebase

--- app/src/main/java/com/example/teamproject/MainActivity.java
+++ app/src/main/java/com/example/teamproject/MainActivity.java
@@ -4,42 +4,27 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.DatePicker;
 import android.widget.ListView;
-import android.widget.RadioButton;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 
-import com.example.teamproject.R;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -116,14 +101,23 @@ public class MainActivity extends AppCompatActivity {
 
 
     private void deleteGoal() {
-        String goalId = databaseReference.push().getKey();
-        DatabaseReference drGaol = FirebaseDatabase.getInstance().getReference("goals").child(goalId);
-
-        drGaol.removeValue();
-        Toast.makeText(this, "Goal accomplished!", Toast.LENGTH_LONG).show();
+//        final DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
+//        ref.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(DataSnapshot snapshot) {
+//                for (DataSnapshot objSnapshot: snapshot.getChildren()) {
+//                    Object obj = snapshot.getKey();
+//                    ref.child(String.valueOf(obj)).removeValue();
+//
+//                }
+//            }
+//            @Override
+//            public void onCancelled(DatabaseError firebaseError) {
+//                Log.e("Read failed", firebaseError.getMessage());
+//            }
+//        });
+        Toast.makeText(this, "Goal accomplished!!",Toast.LENGTH_LONG).show();
     }
-
-
 
 
     // Navigation with the menu
@@ -143,11 +137,7 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent3 = new Intent(this, ProgressGraph.class);
                 this.startActivity(intent3);
                 return true;
-/*            case R.id.friendProgress:
-                Intent intent4 = new Intent(this, SecondFragment.class);
-                this.startActivity(intent4);
-                return true;
-*/            default:
+            default:
                 return super.onOptionsItemSelected(item);
         }
     }
